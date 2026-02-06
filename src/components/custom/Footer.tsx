33588d570b2d560e4c137c497ed22856ff16c716
carapan <122072460+radityapanca02@users.noreply.github.com>
2026-02-06 14:04:55 +0700

    Update footer with attribution text

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -5,6 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-100 dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 mt-8 relative z-10">
       <div className="border-t border-gray-200 dark:border-gray-700 py-4 text-center text-sm text-gray-500 dark:text-gray-400">
+          Provide By Muhammad Panca RAditya Pamungkas<br></br>
           ©{APP.year} {APP.copy}. All rights reserved.
       </div>
     </footer>
